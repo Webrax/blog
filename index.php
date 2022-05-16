e2f3864c26195589702381af005dafccf2a65fba
Malevych Yurii <yuramvm1@gmail.com>
2022-05-16 14:36:21 +0300

    add twig

--- index.php
+++ index.php
@@ -2,19 +2,35 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
 
 require __DIR__ . '/vendor/autoload.php';
 
+// Instantiate App
+$loader = new FilesystemLoader('templates');
+$view = new Environment($loader); // об'єкт для відмалювання шаблонів
+
+// Create app
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+// Add error middleware
+$app->addErrorMiddleware(true, true, true);
+
+// Add routes
+$app->get('/home', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('index.twig');
+    $response->getBody()->write($body);
     return $response;
 });
 
-$app->get('/about', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("About page!");
+$app->get('/about', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('about.twig', [
+        'name' => 'User`s'
+    ]);
+    $response->getBody()->write($body);
     return $response;
 });
+
 
 $app->run();
