4ef4a3e7b1245ca2c3450f6982818b5f0488498c
Malevych Yurii <yuramvm1@gmail.com>
2022-06-01 14:02:21 +0300

    add latest posts

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+
+use Blog\LatestPosts;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -26,8 +28,6 @@ try {
     die();
 }
 
-$postMapper = new PostMapper($connection);
-
 // Create app
 $app = AppFactory::create();
 
@@ -35,19 +35,18 @@ $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 // Add routes
-$app->get('/', function (Request $request, Response $response, $args) use ($view, $postMapper)
-    {
-        $posts = $postMapper->getList('DESC');
+$app->get('/home', function (Request $request, Response $response) use ($view, $connection) {
+    $latestPosts = new LatestPosts($connection);
+    $posts = $latestPosts->get(3);
 
-        $body = $view->render('index.twig', [
-            'posts' => $posts
-        ]);
-        $response->getBody()->write($body);
-        return $response;
-    }
-);
+    $body = $view->render('index.twig', [
+        'posts' => $posts
+    ]);
+    $response->getBody()->write($body);
+    return $response;
+});
 
-$app->get('/about', function (Request $request, Response $response, $args) use ($view) {
+$app->get('/about', function (Request $request, Response $response) use ($view) {
     $body = $view->render('about.twig', [
         'name' => 'User`s'
     ]);
@@ -55,7 +54,8 @@ $app->get('/about', function (Request $request, Response $response, $args) use (
     return $response;
 });
 
-$app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view, $postMapper) {
+$app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view, $connection) {
+    $postMapper = new PostMapper($connection);
     $post = $postMapper->getByUrlKey((string) $args['url_key']);
 
     if (empty($post)) {
--- src/PostMapper.php
+++ src/PostMapper.php
@@ -15,7 +15,7 @@ class PostMapper
     private PDO $connection;
 
     /**
-     * PostMapper constructor
+     * PostMapper constructor.
      * @param PDO $connection
      */
     public function __construct(PDO $connection)
@@ -39,20 +39,20 @@ class PostMapper
         return array_shift($result);
     }
 
+    /**
+     * @param string $direction
+     * @return array|null
+     * @throws Exception
+     */
     public function getList(string $direction): ?array
     {
         if (!in_array($direction, ['DESC', 'ASC'])) {
             throw new Exception('The direction is not supported.');
         }
-        $statement = $this->connection
-            ->prepare(
-                'SELECT * FROM post ORDER BY published_date '
-                . $direction
-            );
+        $statement = $this->connection->prepare('SELECT * FROM post ORDER BY published_date ' . $direction);
 
         $statement->execute();
 
         return $statement->fetchAll();
     }
-
 }
